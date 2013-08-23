62cb9190fc42e068b1b03e1c1e5021854a1d1361
usman-MAC <drusmanbashir@gmail.com>
2013-08-23 09:53:41 +0100

    mac

---
+++ editor/admin.py
@@ -0,0 +1,5 @@
+import models
+from django.contrib import admin
+from django.contrib.auth.models import User
+
+admin.site.register(models.Student,models.Case)
--- editor/models.py
+++ editor/models.py
@@ -2,16 +2,71 @@ from django.db import models
 from django import forms
 import numpy as np
 
-class ContactForm (forms.Form):
- # subject = forms.CharField(max_length=100)
- # message = forms.CharField()
- # sender = forms.EmailField()
-  #cc_myself = forms.BooleanField(required = False)
-  file = forms.FileField()
+class Systems(models.Model) :
+        HN='HN'
+        CHEST='CH'
+        CNS='CN'
+        MSK='MS'
+        PAEDS='PA'
+        GI='GI'
+        GU='GU'
+        VAS='VA'
+        BRE='BR'
+        which_system=(
+            (HN,'Head & Neck'),
+            (BRE,'Breast'),
+            (CHEST,'Chest'),
+            (CNS,'Neuro'),
+            (MSK,'MSK'),
+            (PAEDS,'Paediatrics'),
+            (GI,'Gastrointestinal'),
+            (GU,'Genitourinary'),
+            (VAS,'Vascular'),
+        )
 
+
+class ContactForm (forms.Form):
+    # subject = forms.CharField(max_length=100)
+    # message = forms.CharField()
+    # sender = forms.EmailField()
+    #cc_myself = forms.BooleanField(required = False)
+    file = forms.FileField() 
+    
 class ImageFile (models.Model):
-  imageFile = models.FileField(upload_to='uploaded_images/%Y/%m/%d')
+    imageFile = models.FileField(upload_to='uploaded_images/%Y/%m/%d')
 
 def Img():
-  a = np.arange(100).reshape(10,10)
-  return a
+    a = np.arange(100).reshape(10,10)
+    return a
+
+
+class Case(models.Model):
+    title = models.CharField(max_length=100, db_index=True)
+    description = models.CharField(max_length=300, db_index=True)
+    created = models.DateField(db_index=True, auto_now_add=True)
+    information = models.CharField(max_length=100, db_index=True)
+    system = models.CharField(max_length=2,
+                            choices=Systems.which_system,
+                            #default=CHEST
+                            )
+    #user = models.ManyToManyField(User, blank=True, null=True, through='UserToAuthor')
+
+class Student(models.Model):
+    FRESHMAN = 'FR'
+    SOPHOMORE = 'SO'
+    JUNIOR = 'JR'
+    SENIOR = 'SR'
+    YEAR_IN_SCHOOL_CHOICES = (
+        (FRESHMAN, 'Freshman'),
+        (SOPHOMORE, 'Sophomore'),
+        (JUNIOR, 'Junior'),
+        (SENIOR, 'Senior'),
+    )
+    year_in_school = models.CharField(max_length=2,
+                                      choices=YEAR_IN_SCHOOL_CHOICES,
+                                      default=FRESHMAN)
+
+    def is_upperclass(self):
+        return self.year_in_school in (self.JUNIOR, self.SENIOR)
+
+
