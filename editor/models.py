29c3edc7cadc1982fdd7e11ec841f96bb3c144cd
usman-MAC <drusmanbashir@gmail.com>
2013-08-23 23:51:59 +0100

    models.py at editor

--- editor/models.py
+++ editor/models.py
@@ -51,22 +51,3 @@ class Case(models.Model):
                             )
     #user = models.ManyToManyField(User, blank=True, null=True, through='UserToAuthor')
 
-class Student(models.Model):
-    FRESHMAN = 'FR'
-    SOPHOMORE = 'SO'
-    JUNIOR = 'JR'
-    SENIOR = 'SR'
-    YEAR_IN_SCHOOL_CHOICES = (
-        (FRESHMAN, 'Freshman'),
-        (SOPHOMORE, 'Sophomore'),
-        (JUNIOR, 'Junior'),
-        (SENIOR, 'Senior'),
-    )
-    year_in_school = models.CharField(max_length=2,
-                                      choices=YEAR_IN_SCHOOL_CHOICES,
-                                      default=FRESHMAN)
-
-    def is_upperclass(self):
-        return self.year_in_school in (self.JUNIOR, self.SENIOR)
-
-
