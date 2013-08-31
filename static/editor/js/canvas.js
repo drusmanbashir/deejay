06485ac03f3f47cf72e8687255b35d322b29361c
usman-MAC <drusmanbashir@gmail.com>
2013-08-31 21:13:04 +0100

    wha

---
+++ static/editor/js/canvas.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    var can = document.getElementById("canvas");
+    if (canvas.getContext)
+    {
+
+    var can = document.getElementById("canvas");
+    if (canvas.getContext)
+    {
+    var ctx = can.getContext("2d");
+    cts.fillStyle = "rgb(0,127,0)";
+    ctx.fillRect(10,10,20,20);
+
+}
+    
